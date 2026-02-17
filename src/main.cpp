5f02b249b52454535c194ed0bd24acd5e3ea07cf
Siddharth Praveen Bharadwaj <srsfool@gmail.com>
2026-02-17 21:35:28 +0530

    Minor cursor fix

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,6 @@
 #include "./platform/wii_platform.h"
 #include "./ui/ui_root.h"
 
-static void drawCross(const float x, const float y)
-{
-    GRRLIB_Line(x - 10, y, x + 10, y, 0xFFFFFFFF);
-    GRRLIB_Line(x, y - 10, x, y + 10, 0xFFFFFFFF);
-}
-
 static void pushKeyDown(std::vector<InputEvent>& ev, const Key k)
 {
     ev.push_back({.type = InputEvent::Type::KeyDown, .key = k});
@@ -54,17 +48,13 @@ int main()
         if (quit) break;
         GRRLIB_FillScreen(0x101018FF);
 
-        if (frame.pointer.valid)
-        {
-            drawCross(frame.pointer.x, frame.pointer.y);
-            GRRLIB_Circle(frame.pointer.x, frame.pointer.y, 3, 0xFFCC00FF, true);
-        }
-        else GRRLIB_Rectangle(10, 10, 20, 20, 0xFF0000FF, true);
-
         ui.layout(640, 480);
         ui.update(dt);
         for (const auto& e : events) ui.routeEvent(e);
         ui.draw();
+
+        if (frame.pointer.valid) GRRLIB_Circle(frame.pointer.x, frame.pointer.y, 3, 0xFFCC00FF, true);
+        else GRRLIB_Rectangle(frame.pointer.x - 3, frame.pointer.y - 3, 6, 6, 0xFFCC00FF, true);
 
         videoRender();
     }
